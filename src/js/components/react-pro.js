09eab695fc8cae9b3dd9cf7e90fdd9653cb97be8
Pesanweb Id <andy.yuli@gmail.com>
2024-08-19 17:09:13 +0700

    type reactiv mendapati nilai dari

--- src/js/components/react-pro.js
+++ src/js/components/react-pro.js
@@ -3,37 +3,55 @@ import { LitElement, html, css, nothing } from "lit";
 class ReactPro extends LitElement {
   static properties = {
     name: {
-        type: Boolean,
-    },
-  };
+      // type: Boolean,
+      type: String,
+   
+    converter: {
+      fromAttribute(value, type) {
+        console.log('fromAttribute');
 
-  render() {
-    let template;
-    if (this.name) {
-      template = html`
+        if (value) {
+          return type(value).toLowerCase();
+        }
+        return value;
+      },
+      toAttribute(value, type) {
+        console.log('toAttribute');
+
+        return String(value).toUpperCase();
+      },
+    },
+  },
+
+};
+
+render() {
+  let template;
+  if (this.name) {
+    template = html`
         <p>Selamat datang, ${this.name}</p>
         <p>
           <button @click=${this._logout}>Keluar</button>
         </p>
       `;
-    } else {
-      template = html`
+  } else {
+    template = html`
         <p>Selamat datang, ${this.name}</p>
         <p>
           <button @click=${this._login}>Masuk</button>
         </p>
       `;
-    }
-
-    return template;
   }
 
-  _login() {
-    this.name = 'John Doe';
-  }
-  _logout() {
-    this.name = null;
+  return template;
+}
+
+_login() {
+  this.name = 'John Doe';
+}
+_logout() {
+  this.name = null;
 }
 }
 
-customElements. define('react-pro', ReactPro)
+customElements.define('react-pro', ReactPro)
